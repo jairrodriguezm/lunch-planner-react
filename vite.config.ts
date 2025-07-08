b47cc4c16d17bcb724f95d368fd9a895361a0137
Jair Rodriguez <ing.rodriguezjair@gmail.com>
2025-07-08 13:30:51 -0500

    Fix for Vite and PostCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.cjs'
+    postcss: path.resolve(__dirname, 'postcss.config.cjs'),
   }
 })
